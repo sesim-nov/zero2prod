d8b4ccfd27b44cf6b1ae1196498e9508f3ad7b04
Aaron Moore <aaron@atamisk.net>
2022-10-03 14:33:56 -0400

    Added tests for subscriptions.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,14 @@
 use std::net::TcpListener;
 use zero2prod::run;
 
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind test port");
+    let port = listener.local_addr().unwrap().port();
+    let app = run(listener).expect("Failed to spawn server");
+    let _ = tokio::spawn(app);
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
@@ -21,10 +29,53 @@ async fn health_check_works() {
     assert_eq!(response.content_length(), Some(0));
 }
 
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind test port");
-    let port = listener.local_addr().unwrap().port();
-    let app = run(listener).expect("Failed to spawn server");
-    let _ = tokio::spawn(app);
-    format!("http://127.0.0.1:{}", port)
+#[tokio::test]
+async fn form_post_request_operates_correctly() {
+    //Arrange
+    let app_addr = spawn_app();
+    let client = reqwest::Client::new();
+
+    //Act
+    let body = "name=Test%20User&email=test@example.com";
+    let response = client
+        .post(format!("{}/subscriptions", app_addr))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Sending request failed!");
+
+    //Assert
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn form_post_fails_correctly_with_missing_data() {
+    //Arrange
+    let app_addr = spawn_app();
+    let client = reqwest::Client::new();
+    let bad_requests = vec![
+        ("email=test@example.com", "Missing Name"),
+        ("name=Test%20User", "Missing Email"),
+        ("", "Blank Request"),
+    ];
+
+    for (body, error_message) in bad_requests {
+        //Act
+        let response = client
+            .post(format!("{}/subscriptions", app_addr))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Sending request failed!");
+
+        //Assert
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "API Should have failed with 400 on test: {}",
+            error_message
+        );
+    }
 }
