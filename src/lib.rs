597563af3092354fe8836473dbe0a8b08ccb9e16
Aaron Moore <aaron@atamisk.net>
2022-10-03 00:17:39 -0400

    First test!
    Done for the day. pick back up at 3.5.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,26 @@
-use zero2prod::main;
+use zero2prod::run;
 
-#[test]
-fn dummy_test() {
-    main()
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    // Spawn the web server.
+    spawn_app();
+    // reqwest is needed to comunicate with the server we spawned.
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://localhost:8000/health_check")
+        .send()
+        .await
+        .expect("Request failed to execute.");
+
+    //Assert
+    assert!(response.status().is_success());
+    assert_eq!(response.content_length(), Some(0));
+}
+
+fn spawn_app() {
+    let app = run().expect("Failed to spawn server");
+    let _ = tokio::spawn(app);
 }
