0fc1b650dfcacf5224e9ee70446145980fea7310
Aaron Moore <aaron@atamisk.net>
2022-10-03 00:10:15 -0400

    Made run() asynchronous

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::dev::Server;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -9,14 +10,14 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
+pub fn run() -> std::io::Result<Server> {
+    let srv = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    .run();
+    Ok(srv)
 }
