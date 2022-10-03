648a0ba23ca4bdd5648eb7fffe06028b72eddbc8
Aaron Moore <aaron@atamisk.net>
2022-10-03 09:24:32 -0400

    App listener port can now be arbitrarily specified.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,16 +1,17 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
     // Spawn the web server.
-    spawn_app();
+    let app_addr = spawn_app();
     // reqwest is needed to comunicate with the server we spawned.
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://localhost:8000/health_check")
+        .get(format!("{}/health_check", app_addr))
         .send()
         .await
         .expect("Request failed to execute.");
@@ -20,7 +21,10 @@ async fn health_check_works() {
     assert_eq!(response.content_length(), Some(0));
 }
 
-fn spawn_app() {
-    let app = run().expect("Failed to spawn server");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind test port");
+    let port = listener.local_addr().unwrap().port();
+    let app = run(listener).expect("Failed to spawn server");
     let _ = tokio::spawn(app);
+    format!("http://127.0.0.1:{}", port)
 }
