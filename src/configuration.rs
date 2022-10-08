ae53b43c653ddc3bf47966d4fddfc2619e2b7a30
Aaron Moore <aaron@atamisk.net>
2022-10-08 14:17:58 -0400

    Wired in configuration.

--- src/configuration.rs
+++ src/configuration.rs
@@ -13,3 +13,15 @@ pub struct DatabaseSettings {
     pub host: String,
     pub db_name: String,
 }
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // Setup config reader. 
+    let settings = config::Config::builder()
+        // Add config path at hard-coded config location. 
+        .add_source(
+            config::File::new("configuration.yaml", config::FileFormat::Yaml)
+        )
+        .build()?;
+    //Try to convert config to application config type. 
+    settings.try_deserialize::<Settings>()
+}
