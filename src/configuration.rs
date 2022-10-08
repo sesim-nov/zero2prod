150a3f38fbc84ebb9fcb354d82c5809294521524
Aaron Moore <aaron@atamisk.net>
2022-10-08 14:26:43 -0400

    Added connection string.

--- src/configuration.rs
+++ src/configuration.rs
@@ -14,14 +14,24 @@ pub struct DatabaseSettings {
     pub db_name: String,
 }
 
-pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    // Setup config reader. 
-    let settings = config::Config::builder()
-        // Add config path at hard-coded config location. 
-        .add_source(
-            config::File::new("configuration.yaml", config::FileFormat::Yaml)
+impl DatabaseSettings {
+    pub fn get_connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.db_name
         )
+    }
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // Setup config reader.
+    let settings = config::Config::builder()
+        // Add config path at hard-coded config location.
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
         .build()?;
-    //Try to convert config to application config type. 
+    //Try to convert config to application config type.
     settings.try_deserialize::<Settings>()
 }
