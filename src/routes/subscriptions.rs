3746b9e3cb3e052d545b9ee50918daf474285aec
Aaron Moore <aaron@atamisk.net>
2022-10-08 15:36:31 -0400

    Added an insert query into the subscription logic.
    Identified an issue where I'll need a PgPool instead
    of PgConnection

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,13 +1,28 @@
 use actix_web::{web, HttpResponse, Responder};
+use chrono::Utc;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
-#[allow(dead_code)]
 pub struct FormData {
     email: String,
     name: String,
 }
 
-#[allow(dead_code, unused_variables)]
-pub async fn handle_subscribe(form: web::Form<FormData>) -> impl Responder {
+pub async fn handle_subscribe(
+    form: web::Form<FormData>,
+    db_connection: web::Data<sqlx::PgConnection>,
+) -> impl Responder {
+    let status = sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(db_connection.get_ref())
+    .await;
     HttpResponse::Ok()
 }
