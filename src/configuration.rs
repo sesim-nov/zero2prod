2dfebb9aec459de7e243d8fb013831bdaca5006e
Aaron Moore <aaron@atamisk.net>
2022-10-08 14:03:12 -0400

    More detail on config data types.

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,5 +1,15 @@
 #[allow(dead_code)]
 #[derive(serde::Deserialize)]
 pub struct Settings {
-    app_port: u16,
+    pub app_port: u16,
+    pub database: DatabaseSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub db_name: String,
 }
