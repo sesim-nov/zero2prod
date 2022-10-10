57f38e78ce1fb944d39d330165248b79db227f54
Aaron Moore <aaron@atamisk.net>
2022-10-09 23:09:30 -0400

    most tracing working

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    // Make sure logger logs wind up in the tracer output.
+    tracing_log::LogTracer::init().expect("Failed to set logger");
+
+    // Tracer Setup
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber!");
+
     let configuration = get_configuration().expect("Failed to get configuration");
     let db_connection = PgPool::connect(&configuration.database.get_connection_string())
         .await
