9e7e840cad6b5233a3b6927843852895e8c50828
Aaron Moore <aaron@atamisk.net>
2022-10-10 22:10:13 -0400

    Forgot to cargo fmt...

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,9 @@
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
-use once_cell::sync::Lazy;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 static SUBSCRIBER: Lazy<()> = Lazy::new(|| {
