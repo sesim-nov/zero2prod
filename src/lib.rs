b5967a59a62572916285a7bd4113fcf7e630b175
Aaron Moore <aaron@atamisk.net>
2022-10-21 00:27:42 -0400

    Defined interface for email ops.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub mod configuration;
 pub mod domain;
+pub mod mail;
 pub mod routes;
 pub mod startup;
 pub mod telemetry;
-pub mod mail;
--- src/mail.rs
+++ src/mail.rs
@@ -1,3 +1,24 @@
-pub fn send_mail() -> () {
-    todo!();
+use crate::domain::ListSubscriberEmail;
+
+pub struct EmailMessage {
+    pub recipient: ListSubscriberEmail,
+    pub subject: String,
+    pub body_text: String,
+    pub body_html: String,
+}
+
+pub struct EmailClient {
+    sender: ListSubscriberEmail,
+}
+
+impl EmailClient {
+    pub fn new(sender: ListSubscriberEmail) -> Self {
+        Self { sender }
+    }
+    pub fn get_sender(&self) -> &ListSubscriberEmail {
+        &self.sender
+    }
+    pub async fn send_mail(_message: EmailMessage) -> Result<(), String> {
+        todo!();
+    }
 }
