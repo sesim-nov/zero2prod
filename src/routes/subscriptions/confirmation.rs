f4072d025c5536598ec7afa45623f88356c36b51
Aaron Moore <aaron@atamisk.net>
2022-10-31 23:30:40 -0400

    Documentation

--- src/routes/subscriptions/confirmation.rs
+++ src/routes/subscriptions/confirmation.rs
@@ -5,6 +5,11 @@ pub struct Token {
     pub token: String,
 }
 
+/// Confirm that a user email address is controlled by the initial requestor. 
+///
+/// This endpoint uses the user's subscription token to validate that the user actually controls
+/// the registered e-mail. It handles database management as well as user feedback for the
+/// confirmation. 
 #[allow(clippy::async_yields_async)]
 #[tracing::instrument(name = "Subscriber Confirmation endpoint", skip(_query))]
 pub async fn handle_confirm(_query: web::Query<Token>) -> impl Responder {
