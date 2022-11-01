3f009c2da246df53aa5568f3eb1d4f6ce2510a47
Aaron Moore <aaron@atamisk.net>
2022-10-31 23:33:49 -0400

    More docs

--- src/routes.rs
+++ src/routes.rs
@@ -1,3 +1,7 @@
+//! Endpoints for the REST API exposed by this crate.
+//!
+//! This module contains the handlers for the various endpoints exposed by this application's REST
+//! API.
 mod greet;
 mod health_check;
 mod subscriptions;
