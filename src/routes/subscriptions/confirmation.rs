b7f1b476acf6c84a1538f114812b0c82af7071ee
Aaron Moore <aaron@atamisk.net>
2022-11-12 20:17:40 -0500

    Click test is green!

--- src/routes/subscriptions/confirmation.rs
+++ src/routes/subscriptions/confirmation.rs
@@ -17,9 +17,39 @@ pub async fn handle_confirm(
     query: web::Query<Token>,
     pool: web::Data<sqlx::PgPool>,
 ) -> impl Responder {
-    let _id = match token::get_id_for_token(query.token.clone(), &pool).await {
+    let id = match token::get_id_for_token(query.token.clone(), &pool).await {
         Ok(id) => id,
         Err(_) => return HttpResponse::InternalServerError(),
     };
-    HttpResponse::Ok()
+
+    let id = match id {
+        Some(id) => id,
+        None => {
+            tracing::error!("No such token {} found", query.token);
+            return HttpResponse::Unauthorized();
+        }
+    };
+
+    match confirm_id(id, &pool).await {
+        Ok(_) => {
+            tracing::info!("User confirmation successful!");
+            HttpResponse::Ok()
+        }
+        Err(_) => {
+            tracing::error!("User confirmation query failed!");
+            HttpResponse::InternalServerError()
+        }
+    }
+}
+
+async fn confirm_id(id: uuid::Uuid, pool: &sqlx::PgPool) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        UPDATE subscriptions SET status = 'confirmed' WHERE id = $1
+        "#,
+        id,
+    )
+    .execute(pool)
+    .await?;
+    Ok(())
 }
