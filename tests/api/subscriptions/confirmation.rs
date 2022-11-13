b7bd97a0b0dc1c1ae4b2a198872e3690275d8d1a
Aaron Moore <aaron@atamisk.net>
2022-11-12 20:18:51 -0500

    Remove obsolete test.

--- tests/api/subscriptions/confirmation.rs
+++ tests/api/subscriptions/confirmation.rs
@@ -75,25 +75,6 @@ pub async fn extracted_link_from_confirm_email_returns_200() {
 }
 
 #[tokio::test]
-pub async fn confirm_returns_200_with_valid_data() {
-    // Arrange
-    let app = TestApp::spawn_new().await;
-    //TODO: Token format
-    let token = "TOKEN";
-
-    // Act
-    let response = reqwest::get(format!(
-        "{}:{}/subscriptions/confirm?token={}",
-        app.app_address, app.app_port, token
-    ))
-    .await
-    .unwrap();
-
-    // Assert
-    assert_eq!(response.status().as_u16(), 200);
-}
-
-#[tokio::test]
 pub async fn confirm_returns_400_with_no_token() {
     // Arrange
     let app = TestApp::spawn_new().await;
