b91e8e12b25aa02634c7fa1124c6ea3621c4832c
Aaron Moore <aaron@atamisk.net>
2022-11-12 22:37:55 -0500

    Red test for dual email

--- tests/api/subscriptions/email.rs
+++ tests/api/subscriptions/email.rs
@@ -47,3 +47,27 @@ pub async fn confirmation_email_has_link() {
     let links = app.get_links(email_request);
     assert_eq!(links.html, links.plain_text);
 }
+
+#[tokio::test]
+pub async fn subscribing_twice_sends_dual_emails() {
+    // Arrange
+    let app = TestApp::spawn_new().await;
+    let name = "Fake Name";
+    let email = SafeEmail().fake::<String>();
+    let body = format!("name={}&email={}", name, email);
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(2)
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    for _ in 0..2 {
+        let response = app.post_subscriptions(body.clone()).await;
+        // Assert
+        assert_eq!(response.status(), 200);
+    }
+}
+
